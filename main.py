431d568c9bf31f7e5147a7a387838d3686ae55f6
Татьяна Витальевна <tatianavitalievna@yandex.ru>
2026-06-27 09:37:16 +0300

    Реализована функция play_game()

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from utils import check_winner
+from app_logic import human_turn, computer_turn
+
 def start_game():
     sticks=21
     turn=input("Хотите ходить первым? (да/нет): ").strip().lower()
@@ -7,3 +10,19 @@ def start_game():
         human_first=False
     play_game(human_first,sticks)
 
+def play_game(human_first,sticks):
+    current_player=human_first
+    while sticks>0:
+        if current_player:
+            taken=human_turn(sticks)
+        else:
+            taken=computer_turn(sticks)
+        sticks-=taken
+        current_player=not current_player
+        if check_winner(sticks):
+            winner="Вы победили!" if current_player else "Победил компьютер!"
+            print(winner)
+            break
+if __name__=="__main__":
+    import random
+    start_game()
